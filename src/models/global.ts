b616c59e4c10294579047a503a4d9be72dfbf9bb
luoqiang <709152151@qq.com>
2023-04-26 22:27:50 +0800

    luoqiang

--- src/models/global.ts
+++ src/models/global.ts
@@ -3,7 +3,7 @@ import { DEFAULT_NAME } from '@/constants';
 import { useState } from 'react';
 
 const useUser = () => {
-  const [name, setName] = useState<string>(DEFAULT_NAME);
+  const [name, setName] = useState<string>(DEFAULT_NAME); 
   return {
     name,
     setName,
